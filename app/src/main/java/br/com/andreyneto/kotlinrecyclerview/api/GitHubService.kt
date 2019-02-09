a4ee8187c3bef8bffc3fa715456d21c58c240ba9
Andrey Neto <me@andreyneto.com.br>
2019-02-09 17:12:16 -0200

    Update GitHubService.kt

--- app/src/main/java/br/com/andreyneto/kotlinrecyclerview/api/GitHubService.kt
+++ app/src/main/java/br/com/andreyneto/kotlinrecyclerview/api/GitHubService.kt
@@ -2,9 +2,11 @@ package br.com.andreyneto.kotlinrecyclerview.api
 
 import br.com.andreyneto.kotlinrecyclerview.model.GitHubRepositoriesResult
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GitHubService {
+    @GET("search/repositories")
     fun searchRepositories(
         @Query("q") query: String,
         @Query("sort") sort: String = "stars",
