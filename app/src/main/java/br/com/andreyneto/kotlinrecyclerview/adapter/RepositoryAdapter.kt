aa1c241559d854b670c89c60723336080bfe3fe0
Andrey Neto <me@andreyneto.com.br>
2019-02-09 16:43:13 -0200

    Create RepositoryAdapter.kt

---
+++ app/src/main/java/br/com/andreyneto/kotlinrecyclerview/adapter/RepositoryAdapter.kt
@@ -0,0 +1,42 @@
+package br.com.andreyneto.kotlinrecyclerview.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import br.com.andreyneto.kotlinrecyclerview.R
+import br.com.andreyneto.kotlinrecyclerview.model.ProgrammingLanguage
+import br.com.andreyneto.kotlinrecyclerview.model.Repository
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.programming_language_item.view.*
+
+class RepositoryAdapter(
+    private val items: List<Repository>,
+    private val context: Context,
+    private val listener: (ProgrammingLanguage) -> Unit
+) : Adapter<RepositoryAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+         ViewHolder(
+            LayoutInflater.from(context)
+                .inflate(R.layout.programming_language_item, parent, false))
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindView(items[position], listener)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bindView(item: Repository,
+                     listener: (ProgrammingLanguage) -> Unit) = with(itemView){
+            Glide.with(ivMain.context).load(item).into(ivMain)
+            tvTitle.text = item.title
+            tvLaunchYear.text = item.year.toString()
+            tvDescription.text = item.description
+            setOnClickListener { listener(item) }
+        }
+    }
+}
